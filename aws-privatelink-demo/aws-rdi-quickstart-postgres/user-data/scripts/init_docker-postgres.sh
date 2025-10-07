380aea0537034bd5a86f1a8be0283f9541b18b09
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2025-10-07 11:39:38 -0400

    Remove unused python load gen

--- aws-privatelink-demo/aws-rdi-quickstart-postgres/user-data/scripts/init_docker-postgres.sh
+++ aws-privatelink-demo/aws-rdi-quickstart-postgres/user-data/scripts/init_docker-postgres.sh
@@ -8,4 +8,3 @@ echo "wal_level = logical" >> $PGDATA/postgresql.conf
 
 psql -U postgres postgres -f ~/chinook_create.sql
 psql -U postgres chinook -f ~/Chinook_PostgreSql.sql
-psql -U postgres chinook -f ~/create_debezium_user.sql
