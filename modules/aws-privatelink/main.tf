27bbae5b957ff170ccbf5801b865ff30446efdca
Alysha Gardner <3100188+actgardner@users.noreply.github.com>
2026-05-14 10:39:08 -0400

    RDSC-5235: Reject PrivateLink connection when tearing down database

--- modules/aws-privatelink/main.tf
+++ modules/aws-privatelink/main.tf
@@ -20,4 +20,17 @@ resource "aws_vpc_endpoint_service" "producer_service" {
   tags = {
     Name = "producer-service-${var.identifier}"
   }
+
+  provisioner "local-exec" {
+    when = destroy
+    environment = {
+      "AWS_REGION" = split(":", self.arn)[3]
+    }
+    command = <<-EOF
+ENDPOINT_ID="$(aws ec2 describe-vpc-endpoint-connections --query "VpcEndpointConnections[? VpcEndpointId!=null].VpcEndpointId | [0] || ''"  --filter Name=service-id,Values=${self.id} --output text)"
+if [ -n "$ENDPOINT_ID" ]; then
+  aws ec2 reject-vpc-endpoint-connections --service-id ${self.id} --vpc-endpoint-ids $ENDPOINT_ID
+fi
+EOF
+  }
 }
